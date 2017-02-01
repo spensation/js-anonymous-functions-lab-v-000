34fff3bd9fcca621236368a5bde425601d92bba3
Spencer Evans <spencevans@gmail.com>
2017-02-01 02:00:21 +0000

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-  menu();
-  play();
-  pause();
-  show();
-});
 
 var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
 var artists = Object.keys(jukeboxSongs);
@@ -15,30 +9,49 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', handleMenuClick);
+}
+
+function handleMenuClick(e) {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', playOnClick);
+}
+
+function playOnClick(e) {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', pauseOnClick);
+}
+
+function pauseOnClick(e) {
+  $('#songPaused').html(songToPlay + " is paused");
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', showOnClick);
 }
+
+function showOnClick(e) {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
+}
+
+$(document).ready(function(){
+  var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
+  var artists = Object.keys(jukeboxSongs);
+  var theArtist = artists[Math.floor(Math.random()) * artists.length];
+  var songToPlay = jukeboxSongs[theArtist];
+  menu();
+  play();
+  pause();
+  show();
+});
